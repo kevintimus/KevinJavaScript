2efadb82d8ce7f5283e7c87a15abda99c0ef0fff
Kevin T <kevin.timus@institutvidreres.cat>
2025-11-10 11:11:51 +0100

    Ex1

---
+++ JavaScriptEx/Ex1.js
@@ -0,0 +1,33 @@
+//Ex1
+console.log("Kevin")
+console.warn("Timus")
+console.error("6/9/2007")
+
+//Ex2
+let Nom = String;
+let Edad = 18;
+let Familia = true;
+console.log(Nom);
+console.log(Edad);
+console.log(Familia);
+
+//Ex3
+console.log(Hola)
+var Hola = "Hola"
+
+console.log(Hola2)
+var Hola2 = "Hola"
+
+
+//Ex4
+let variable = 100;
+console.log(variable);
+variable = "cien";
+console.log(variable);
+
+//Ex5
+const hola = "Hola";
+console.log(hola);
+hola = 12;
+console.log(hola)
+
---
+++ JavaScriptEx/Ex2.js
@@ -0,0 +1,48 @@
+//Ex6
+let num = 1.234;
+let big = 5678;
+console.log(num + big)
+console.log(num - big)
+console.log(num / big)
+console.log(num * big)
+
+//Ex 7
+let altura = 4;
+let base = 8;
+let hipotenusa = ((altura * altura) + (base * base))
+console.log(Math.sqrt(hipotenusa));
+
+//Ex 8
+let enter = 1;
+let string = "Hola";
+console.log(enter / string)
+
+//Ex 9
+let nomcognom = "Kevin Timus Leonte"
+let primercognom = nomcognom.slice(6,11);
+console.log(primercognom.length)
+
+//Ex 10
+nomcognom = "Kevin Timus Leonte"
+primercognom = nomcognom.slice(0, 6) + nomcognom.slice(7);
+console.log(primercognom)
+
+//Ex 11
+var x = null;
+var a = undefined;
+console.log(x + a)
+
+//Ex 12
+let bool = true
+console.log("El valor és " + bool);
+console.log(String(bool));
+
+//Ex 13
+let string1 = "Hola"
+console.log(!!string1)
+console.log(Boolean(string1))
+
+//Ex 14
+bool = true
+console.log(2 + bool);
+console.log(Number(bool));
